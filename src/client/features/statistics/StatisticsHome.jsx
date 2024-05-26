f467d04722b0a8c8221bce29a3d18d1d890159db
ianbraverman@hotmail.com <ianbraverman@hotmail.com>
2024-05-26 12:42:53 -0400

    created the assets vs liabilities breakdown function and am calling it in the main exported function now

--- src/client/features/statistics/StatisticsHome.jsx
+++ src/client/features/statistics/StatisticsHome.jsx
@@ -19,9 +19,9 @@ function YearlyIncomeExpenses({me}) {
     if (surplusDeficit >= 0) {
         return (
             <>
-                <p>Great job, your yearly income of {totalIncome} exceeds your yearly expenses of {totalExpenses}. Your yearly surplus is {surplusDeficit}</p>
+                <p>Great job, your yearly income of {totalIncome} exceeds or covers your yearly expenses of {totalExpenses}. Your yearly surplus is {surplusDeficit}</p>
                 <p>You are earning enough to cover your yearly expenses</p>
-                <p>For a more detailed breakdown please press here</p>
+                <p>For a more detailed breakdown press the button below</p>
                 <button>Income Expenses Breakdown</button>
             </>
         )
@@ -29,11 +29,43 @@ function YearlyIncomeExpenses({me}) {
         return ( 
             <>
                 <p>Due to your yearly expenses of {totalExpenses} exceeding your yearly income of {totalIncome}, you are currently running a deficit of {surplusDeficit}</p>
-                <p>There are many ways to manage your expenses. For a more detailed breakdown please press here</p>
+                <p>There are many ways to manage your expenses. For a more detailed breakdown press the button below</p>
                 <button>Income Expenses Breakdown</button>
             </>
         )
     }
+}
+
+function CurrentAssetsLiabilities({me}) {
+    let totalAssets = 0
+    let totalLiabilities = 0
+    for (let i = 0; i<me.Assets.length; i++) {
+        totalAssets += me.Assets[i].balance;
+    }
+    for (let i = 0; i<me.Liabilities.length; i++) {
+        totalLiabilities += me.Liabilities[i].amount;
+    }
+    let breakdown = totalAssets - totalLiabilities;
+    if (breakdown >=0) {
+        return (
+            <>
+                <p>Great job, your overall assets of {totalAssets} exceeds your overall liabilities of {totalLiabilities}</p>
+                <p>You have done a nice job of not taking on too much debt, and saving enough.</p>
+                <p>For a more detailed breakdown press the button below</p>
+                <button>Assets Liabilities Breakdown</button>
+            </>
+        )
+    } else {
+        return (
+            <>
+                <p>Your total liabilities of {totalLiabilities} is currently exceeding your overall assets of {totalAssets}</p>
+                <p>There are many ways to decrease your liabilities and increase your assets. For a more detailed breakdown press the button below</p>
+                <button>Assets Liabilities Breakdown</button>
+            </>
+        )
+    }
+
+
 }
 
 
@@ -50,6 +82,7 @@ export default function StatisticsHome() {
         <> 
         <h1>Here is a breakdown of some of the key aspects of your financial wellness</h1>
         {me && <YearlyIncomeExpenses me={me} />}
+        {me && <CurrentAssetsLiabilities me={me} />}
         </>
     ) : (
         <p>Please Log In</p>
